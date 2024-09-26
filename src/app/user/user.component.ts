9dee8c112cbee8fd5320a625f03c13f312bfacfc
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-09-26 09:24:55 +0100

    implement *ngFor Directive

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,15 +7,32 @@ import  {User } from './user'
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-user : User ={
-"id":1 ,
+  users:any;
+
+// user : User ={
+// "id":1 ,
+// "name": "Abdrrahmane ",
+// "email":"abderrahmane.boukhezar@gmail.com",
+// "mobile":"+213664065859",
+
+// }
+
+  constructor() {
+    this .users=[
+      {
+      "id":1 ,
 "name": "Abdrrahmane ",
 "email":"abderrahmane.boukhezar@gmail.com",
 "mobile":"+213664065859",
-
-}
-
-  constructor() { }
+      },
+      {
+        "id":2 ,
+  "name": "Abdrrahmane ",
+  "email":"abderrahmane.boukhezar@gmail.com",
+  "mobile":"+213664065859",
+        }
+    ]
+   }
 
   ngOnInit() {
   }
